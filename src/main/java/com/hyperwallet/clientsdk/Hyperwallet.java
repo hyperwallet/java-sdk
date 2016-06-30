6c4f206205d252be528417e580863b72f91d950b
Florian Krauthan <fkrauthan@hyperwallet.com>
2016-06-30 13:43:32 -0700

    Prepared initial release

--- src/main/java/com/hyperwallet/clientsdk/Hyperwallet.java
+++ src/main/java/com/hyperwallet/clientsdk/Hyperwallet.java
@@ -17,7 +17,7 @@ import java.util.TimeZone;
  */
 public class Hyperwallet {
 
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.1.0";
 
     private final HyperwalletApiClient apiClient;
     private final String programToken;
