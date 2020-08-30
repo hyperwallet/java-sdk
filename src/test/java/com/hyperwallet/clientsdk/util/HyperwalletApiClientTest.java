0c0a18ce39b3fff81ccfbf625b7f796a450df3cb
ramahalingam <ramahalingam@paypal.com>
2020-08-30 22:56:03 +0530

    v3 changed to v4

--- src/test/java/com/hyperwallet/clientsdk/util/HyperwalletApiClientTest.java
+++ src/test/java/com/hyperwallet/clientsdk/util/HyperwalletApiClientTest.java
@@ -881,7 +881,7 @@ public class HyperwalletApiClientTest {
                                         "        \"params\" : {\n" +
                                         "          \"rel\" : \"self\"\n" +
                                         "        },\n" +
-                                        "        \"href\" : \"https://maelle.paylution.com/rest/v3/payments/pmt-81aad61a-03ff-4995-a2fc-6a6e2d8911af\"\n" +
+                                        "        \"href\" : \"https://maelle.paylution.com/rest/v4/payments/pmt-81aad61a-03ff-4995-a2fc-6a6e2d8911af\"\n" +
                                         "      } ]\n" +
                                         "    }    {\n" +
                                         "      \"token\" : \"pmt-81aad61a-03ff-4995-a2fc-6a6e2d8911af\",\n" +
@@ -898,7 +898,7 @@ public class HyperwalletApiClientTest {
                                         "        \"params\" : {\n" +
                                         "          \"rel\" : \"self\"\n" +
                                         "        },\n" +
-                                        "        \"href\" : \"https://maelle.paylution.com/rest/v3/payments/pmt-81aad61a-03ff-4995-a2fc-6a6e2d8911af\"\n" +
+                                        "        \"href\" : \"https://maelle.paylution.com/rest/v4/payments/pmt-81aad61a-03ff-4995-a2fc-6a6e2d8911af\"\n" +
                                         "      } ]\n" +
                                         "    }")
         );
