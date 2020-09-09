801e2b21389847df90cd7870efcd0868aa950206
rrathinasabapath <rrathinasabapath@paypal.com>
2020-09-09 22:12:35 +0530

    Add addField method calls and clearToken methods  in HyperWalletProgram

--- src/main/java/com/hyperwallet/clientsdk/model/HyperwalletProgram.java
+++ src/main/java/com/hyperwallet/clientsdk/model/HyperwalletProgram.java
@@ -1,12 +1,16 @@
 package com.hyperwallet.clientsdk.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.hyperwallet.clientsdk.util.HyperwalletJsonConfiguration;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 import java.util.List;
 
+@JsonFilter(HyperwalletJsonConfiguration.INCLUSION_FILTER)
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class HyperwalletProgram extends HyperwalletBaseMonitor{
@@ -22,11 +26,19 @@ public class HyperwalletProgram extends HyperwalletBaseMonitor{
     }
 
     public void setToken(String token) {
+        addField("token", token);
         this.token = token;
     }
 
     public HyperwalletProgram token(String token) {
+        addField("token", token);
         this.token = token;
+        return this;
+    }
+
+    public HyperwalletProgram clearToken() {
+        clearField("token");
+        this.token = null;
         return this;
     }
 
@@ -35,11 +47,19 @@ public class HyperwalletProgram extends HyperwalletBaseMonitor{
     }
 
     public void setCreatedOn(Date createdOn) {
+        addField("createdOn", createdOn);
         this.createdOn = createdOn;
     }
 
     public HyperwalletProgram createdOn(Date createdOn) {
+        addField("createdOn", createdOn);
         this.createdOn = createdOn;
+        return this;
+    }
+
+    public HyperwalletProgram clearCreatedOn() {
+        clearField("createdOn");
+        this.createdOn = null;
         return this;
     }
 
@@ -48,11 +68,19 @@ public class HyperwalletProgram extends HyperwalletBaseMonitor{
     }
 
     public void setName(String name) {
+        addField("name", name);
         this.name = name;
     }
 
     public HyperwalletProgram name(String name) {
+        addField("name", name);
         this.name = name;
+        return this;
+    }
+
+    public HyperwalletProgram clearName() {
+        clearField("name");
+        this.name = null;
         return this;
     }
 
@@ -61,13 +89,22 @@ public class HyperwalletProgram extends HyperwalletBaseMonitor{
     }
 
     public void setParentToken(String parentToken) {
+        addField("parentToken", parentToken);
         this.parentToken = parentToken;
     }
 
     public HyperwalletProgram parentToken(String parentToken) {
+        addField("parentToken", parentToken);
         this.parentToken = parentToken;
         return this;
     }
+
+    public HyperwalletProgram clearParentToken() {
+        clearField("parentToken");
+        this.parentToken = null;
+        return this;
+    }
+
     public List<HyperwalletLink> getLinks() {
         return links;
     }
