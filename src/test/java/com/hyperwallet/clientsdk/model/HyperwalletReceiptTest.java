a5364de4a6274a90f4702d7f28a7cfbc08b08385
Aparna Rao <amylavarapu@paypal.com>
2020-09-08 20:36:16 -0700

    Code changes for Receipts in different branch

--- src/test/java/com/hyperwallet/clientsdk/model/HyperwalletReceiptTest.java
+++ src/test/java/com/hyperwallet/clientsdk/model/HyperwalletReceiptTest.java
@@ -5,8 +5,6 @@ import org.testng.annotations.Test;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -19,7 +17,6 @@ public class HyperwalletReceiptTest {
     @Test
     public void testHyperwalletReceipt() {
         HyperwalletReceipt receipt = new HyperwalletReceipt();
-        assertThat(receipt.getToken(), is(nullValue()));
         assertThat(receipt.getJournalId(), is(nullValue()));
         assertThat(receipt.getType(), is(nullValue()));
         assertThat(receipt.getCreatedOn(), is(nullValue()));
@@ -33,21 +30,12 @@ public class HyperwalletReceiptTest {
         assertThat(receipt.getForeignExchangeCurrency(), is(nullValue()));
         assertThat(receipt.getDetails(), is(notNullValue()));
         assertThat(receipt.getDetails().isEmpty(), is(equalTo(true)));
-        assertThat(receipt.getLinks(), is(nullValue()));
 
-        Map<String, String> detailsMap = new HashMap<>();
+        Map<String, String> detailsMap = new HashMap<String, String>();
         detailsMap.put(HyperwalletReceipt.DetailFieldKey.BANK_NAME.key(), "test");
-        HyperwalletLink link = new HyperwalletLink();
-        link.setHref("https://localhost:8181/test");
-        Map<String, String> rel = new HashMap<>();
-        rel.put("rel","self");
-        link.setParams(rel);
-        List<HyperwalletLink> links = new ArrayList<HyperwalletLink>();
-        links.add(link);
 
         Date creationDate = new Date();
 
-        receipt.setToken("test-token");
         receipt.setJournalId("test-journal-id");
         receipt.setType(HyperwalletReceipt.Type.ANNUAL_FEE);
         receipt.setCreatedOn(creationDate);
@@ -60,9 +48,7 @@ public class HyperwalletReceiptTest {
         receipt.setForeignExchangeRate(0.99);
         receipt.setForeignExchangeCurrency("USD");
         receipt.setDetails(detailsMap);
-        receipt.setLinks(links);
 
-        assertThat(receipt.getToken(), is(equalTo("test-token")));
         assertThat(receipt.getJournalId(), is(equalTo("test-journal-id")));
         assertThat(receipt.getType(), is(equalTo(HyperwalletReceipt.Type.ANNUAL_FEE)));
         assertThat(receipt.getCreatedOn(), is(equalTo(creationDate)));
@@ -75,8 +61,6 @@ public class HyperwalletReceiptTest {
         assertThat(receipt.getForeignExchangeRate(), is(equalTo(0.99)));
         assertThat(receipt.getForeignExchangeCurrency(), is(equalTo("USD")));
         assertThat(receipt.getDetails(), is(equalTo(detailsMap)));
-        assertThat(receipt.getLinks().get(0).getHref(), is(equalTo(link.getHref())));
-        assertThat(receipt.getLinks().get(0).getParams(), is(equalTo(link.getParams())));
     }
 
 }
