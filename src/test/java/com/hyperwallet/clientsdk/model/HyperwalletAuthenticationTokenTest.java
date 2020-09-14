4af071efa0cb50c10def8ce0edc15296a3dda3dc
Aparna Rao <amylavarapu@paypal.com>
2020-09-14 09:40:54 -0700

    Test for Authentication token

---
+++ src/test/java/com/hyperwallet/clientsdk/model/HyperwalletAuthenticationTokenTest.java
@@ -0,0 +1,27 @@
+package com.hyperwallet.clientsdk.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+public class HyperwalletAuthenticationTokenTest{
+
+    @Test
+    public void testAuthenticationToken() {
+
+        HyperwalletAuthenticationToken authenticationToken = new HyperwalletAuthenticationToken();
+        assertThat(authenticationToken.getValue(), is(nullValue()));
+
+      //  return authenticationToken;
+    }
+
+//    protected Class<HyperwalletAuthenticationToken> createModelClass() {
+//        return HyperwalletAuthenticationToken.class;
+//    }
+}
