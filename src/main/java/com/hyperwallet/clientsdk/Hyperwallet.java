56293db5baefa8059e908f77a334fd77d11c134d
Aparna Rao <amylavarapu@paypal.com>
2020-09-14 16:54:15 -0700

    updated version of SDK             b

--- src/main/java/com/hyperwallet/clientsdk/Hyperwallet.java
+++ src/main/java/com/hyperwallet/clientsdk/Hyperwallet.java
@@ -20,7 +20,7 @@ import java.util.TimeZone;
  */
 public class Hyperwallet {
 
-    public static final String VERSION = "1.4.2";
+    public static final String VERSION = "2.0.0";
     private final HyperwalletApiClient apiClient;
     private final String programToken;
     private final String url;
