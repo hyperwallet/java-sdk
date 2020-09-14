4333eee0f85474d8f2edef388f275b8955eb7c69
Suguna Sangaran <ssangaran@paypal.com>
2020-09-14 16:52:57 +0530

    Update for Code Coverage

--- src/main/java/com/hyperwallet/clientsdk/model/HyperwalletAuthenticationToken.java
+++ src/main/java/com/hyperwallet/clientsdk/model/HyperwalletAuthenticationToken.java
@@ -13,4 +13,8 @@ public class HyperwalletAuthenticationToken {
     public String getValue() {
         return value;
     }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
