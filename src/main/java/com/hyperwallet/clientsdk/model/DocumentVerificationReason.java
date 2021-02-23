31e6c8786a5bfeb08529adc81eb51bb476c1b372
Simran Singh <simrandsingh@paypal.com>
2021-02-23 13:44:02 -0800

    Update DocumentVerificationReason. DTONBWTWO-132

--- src/main/java/com/hyperwallet/clientsdk/model/DocumentVerificationReason.java
+++ src/main/java/com/hyperwallet/clientsdk/model/DocumentVerificationReason.java
@@ -1,28 +1,23 @@
 package com.hyperwallet.clientsdk.model;
 
 public enum DocumentVerificationReason {
-    NONE("EKYCDocumentRejectReason.NONE"),
-    DOCUMENT_EXPIRED("EKYCDocumentRejectReason.DOCUMENT_EXPIRED"),
-    DOCUMENT_NOT_SUPPORTED("EKYCDocumentRejectReason.DOCUMENT_NOT_SUPPORTED"),
-    TIMEOUT_OR_DOCUMENT_NOT_SUPPORTED("EKYCDocumentRejectReason.TIMEOUT_OR_DOCUMENT_NOT_SUPPORTED"),
-    DOCUMENT_NOT_RELATED_TO_USER("EKYCDocumentRejectReason.DOCUMENT_NOT_RELATED_TO_USER"),
-    DOCUMENT_NOT_READABLE("EKYCDocumentRejectReason.DOCUMENT_NOT_READABLE"),
-    DOCUMENT_NOT_DECISIVE("EKYCDocumentRejectReason.DOCUMENT_NOT_DECISIVE"),
-    DOCUMENT_SUSPICIOUS("EKYCDocumentRejectReason.DOCUMENT_SUSPICIOUS"),
-    DOCUMENT_VENDOR_UPLOAD_FAILED("EKYCDocumentRejectReason.DOCUMENT_VENDOR_UPLOAD_FAILED"),
-    DOCUMENT_NOT_COMPLETE("EKYCDocumentRejectReason.DOCUMENT_NOT_COMPLETE"),
-    DOCUMENT_CORRECTION_REQUIRED("EKYCDocumentRejectReason.DOCUMENT_CORRECTION_REQUIRED"),
-    DOCUMENT_NOT_VALID_WITH_NOTES("EKYCDocumentRejectReason.DOCUMENT_NOT_VALID_WITH_NOTES"),
-    DOCUMENT_TYPE_NOT_VALID("EKYCDocumentRejectReason.DOCUMENT_TYPE_NOT_VALID"),
-    OTHER("EKYCDocumentRejectReason.OTHER");
+    DOCUMENT_EXPIRED("Document in expired"),
+    DOCUMENT_NOT_RELATED_TO_PROFILE("Document does not match account information"),
+    DOCUMENT_NOT_READABLE("Document is not readable"),
+    DOCUMENT_NOT_DECISIVE("Decision cannot be made based on document. Alternative document required"),
+    DOCUMENT_NOT_COMPLETE("Document is incomplete"),
+    DOCUMENT_CORRECTION_REQUIRED("Document requires correction"),
+    DOCUMENT_NOT_VALID_WITH_NOTES("Document is invalid and rejection details are noted on the account"),
+    DOCUMENT_TYPE_NOT_VALID("Document type is not acceptable");
 
-    private String contentKeyName;
 
-    DocumentVerificationReason(String contentKeyName) {
-        this.contentKeyName = contentKeyName;
+    private final String description;
+
+    DocumentVerificationReason(String description) {
+        this.description = description;
     }
 
-    public String getContentKeyName() {
-        return contentKeyName;
+    public String getDescription() {
+        return description;
     }
 }
