b656b317ffdb95e94d18d58a9c7ce4f780d26da1
Rajendran Rathinasabapathy <rrathinasabapath@paypal.com>
2021-04-30 19:15:45 +0530

    Updated unit test testFromJson_byClassReference_Invalid_JSON_Content

--- src/test/java/com/hyperwallet/clientsdk/util/HyperwalletJsonUtilTest.java
+++ src/test/java/com/hyperwallet/clientsdk/util/HyperwalletJsonUtilTest.java
@@ -71,9 +71,11 @@ public class HyperwalletJsonUtilTest {
         assertThat(body, is(nullValue()));
     }
 
-    @Test(expectedExceptions = HyperwalletException.class)
+    //(expectedExceptions = HyperwalletException.class)
+    @Test
     public void testFromJson_byClassReference_Invalid_JSON_Content() {
-        HyperwalletJsonUtil.fromJson("{\"amount\": \"1,023.37\" }", TestBody.class);
+        TestBody body = qHyperwalletJsonUtil.fromJson("{\"amount\": \"1,023.37\" }", TestBody.class);
+        assertThat(body.amount, is(1023.37));
     }
 
     @Test
