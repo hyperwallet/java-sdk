af9ee5f1bea919a27318feaa8105473b70ea8ca1
Hakim Lahlou <hlahlou@paypal.com>
2022-02-15 13:08:04 -0800

    removed type

--- src/test/java/com/hyperwallet/clientsdk/util/HyperwalletApiClientTest.java
+++ src/test/java/com/hyperwallet/clientsdk/util/HyperwalletApiClientTest.java
@@ -1430,7 +1430,7 @@ public class HyperwalletApiClientTest {
             hyperwalletApiClient.put(baseUrl + "/test?test-query=test-value", requestBody, TestBody.class);
             fail("Expect HyperwalletException");
         } catch (HyperwalletException e) {
-            assertThat(e.getMessage(), is(containsString("java.net.UnknownHostException: localhost:"g)));
+            assertThat(e.getMessage(), is(containsString("java.net.UnknownHostException: localhost:")));
         }
     }
 
