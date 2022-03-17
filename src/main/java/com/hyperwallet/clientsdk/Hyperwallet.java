19baf86c2a3d20eab73cbc5920e52c162bf81f1c
Hakim Lahlou <hlahlou@paypal.com>
2022-03-17 15:21:30 -0700

    changed readme, logs, and version

--- src/main/java/com/hyperwallet/clientsdk/Hyperwallet.java
+++ src/main/java/com/hyperwallet/clientsdk/Hyperwallet.java
@@ -17,7 +17,7 @@ import java.util.*;
  */
 public class Hyperwallet {
 
-    public static final String VERSION = "2.4.1";
+    public static final String VERSION = "2.4.2";
     private final HyperwalletApiClient apiClient;
     private final String programToken;
     private final String url;
